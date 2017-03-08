b65da7b9a07efa26a22494277007239e4373d27c
ldevernay <boblehamster@yahoo.fr>
2017-03-08 10:08:20 +0100

    clean Square

--- src/Square.js
+++ src/Square.js
@@ -3,12 +3,6 @@ import React, { Component } from 'react';
 
 
 class Square extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
