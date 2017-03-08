6bc03ec007427d52c63c2254fd7360c986dde1b5
ldevernay <boblehamster@yahoo.fr>
2017-03-08 15:45:53 +0100

    history display

--- src/Game.js
+++ src/Game.js
@@ -23,6 +23,17 @@ class Game extends Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Move #' + move :
+      'Game start';
+      return (
+        <li key={move}>
+        <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
     return (
       <div className="game">
       <div className="game-board">
@@ -30,7 +41,7 @@ class Game extends Component {
       </div>
       <div className="game-info">
       <div>{status}</div>
-      <ol>{/* TODO */}</ol>
+      <ol>{moves}</ol>
       </div>
       </div>
     );
